76725362f52f9f0e23ab009361754eef870e6a50
maxim <maxime.bos@devinci.fr>
2019-03-22 19:36:56 +0100

    GraphQL endpoints to implement

--- src/graphQL.js
+++ src/graphQL.js
@@ -44,12 +44,55 @@ var schema = buildSchema(`
 
 // The root provides a resolver function for each API endpoint
 var root = {
-    populate: async (source, args) => {
+    populate: async () => {
         const movies = await populate(DENZEL_IMDB_ID);
         const insertion = await collection.insertMany(movies);
         return {
             total: insertion.movie.n
         };
     },
+
+    random: async () => {
+        let options = {
+            "limit": 1,
+            "skip": Math.floor(Math.random() * await collection.countDocuments({"metascore": {$gte: 70}}))
+        }
+        let query = {
+            "metascore": { $gte: 70}
+        };
+        return await collection.findOne(query, options);
+    },
+
+    getMovie: async (args) => {
+        return await collection.findOne({"id": args.id})
+    },
+
+    getMovies: async (args) => {
+        let options = {"limit": args.limit, "sort": [ ['metascore', 'desc']]
+        };
+        return await collection.find({"metascore": {$gte: args.metascore}}, options).toArray();
+    },
+
+    updateMovie: async (args) => {
+        const post =  await collection.updateMany({"id": args.id}, {$set: {review: args.review, date: args.date}}, {"upsert": true});
+        return await collection.findOne({"id": args.id});
+    }
 };
+
+async function populate(actor) {
+    try {
+        console.log(📽️ fetching filmography of ${actor}...);
+        return await imdb(actor);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+let app = express();
+app.use('/graphQL', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiQL: true
+}));
+
 
